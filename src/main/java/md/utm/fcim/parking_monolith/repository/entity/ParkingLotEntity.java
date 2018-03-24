7121d9029a606521abf49ad8b37ea0eec9478093
vadimeladii <vadimeladii@gmail.ru>
2018-03-24 12:59:59 +0200

    Migrate to java 8

--- src/main/java/md/utm/fcim/parking_monolith/repository/entity/ParkingLotEntity.java
+++ src/main/java/md/utm/fcim/parking_monolith/repository/entity/ParkingLotEntity.java
@@ -3,7 +3,10 @@ package md.utm.fcim.parking_monolith.repository.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
 
 /**
  * Created by veladii on 03/18/2018
